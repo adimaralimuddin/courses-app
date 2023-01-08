393dd153c76e4e4787bdf58efb95cf843d6ea2da
adimar <adimaraisha@gmail.com>
2023-01-08 19:09:07 +0800

    set reqest url to absolute path

--- features/api/gqlFetch.ts
+++ features/api/gqlFetch.ts
@@ -9,9 +9,10 @@ export default async function gqlFetch(
     ${gql_}
   `;
   const res = await request(
-    process.env.NODE_ENV === "production"
-      ? process.env.AUTH0_BASE_URL + "/api/graphql"
-      : "http://localhost:3000/api/graphql",
+    "/api/graphql",
+    // process.env.NODE_ENV === "production"
+    //   ? process.env.AUTH0_BASE_URL + "/api/graphql"
+    //   : "http://localhost:3000/api/graphql",
     q,
     data
   );
