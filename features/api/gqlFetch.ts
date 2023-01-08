6e392216c44368d9ab154364e40151a7a34d013b
adimar <adimaraisha@gmail.com>
2023-01-08 18:46:45 +0800

    resolve gqlFetch url to production if production

--- features/api/gqlFetch.ts
+++ features/api/gqlFetch.ts
@@ -9,7 +9,9 @@ export default async function gqlFetch(
     ${gql_}
   `;
   const res = await request(
-    process.env.NEXT_PUBLIC_BASE_URL + "/api/graphql",
+    process.env.NODE_ENV === "production"
+      ? process.env.AUTH0_BASE_URL + "/api/graphql"
+      : "http://localhost:3000/api/graphql",
     q,
     data
   );
